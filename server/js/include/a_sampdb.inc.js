c065518ac5b215dab638a00a3585ad08f85454b4
Laronic <Larsey123@hotmail.no>
2015-07-27 00:03:51 +0200

    Updated js include files

--- server/js/include/a_sampdb.inc.js
+++ server/js/include/a_sampdb.inc.js
@@ -4,20 +4,20 @@
  * @param {String} name
  * @return {Number} retval
  */
-function db_open( name ){
-	var out = CallNativeGDK( "db_open", "s", name );
-	return out;
+function db_open(name) {
+	return CallNativeGDK("db_open", "s", name);
 }
+
 /**
  * db_close
  * @see https://wiki.sa-mp.com/wiki/db_close
  * @param {Number} db
  * @return {Number} retval
  */
-function db_close( db ){
-	var out = CallNativeGDK( "db_close", "i", db );
-	return out;
+function db_close(db) {
+	return CallNativeGDK("db_close", "i", db);
 }
+
 /**
  * db_query
  * @see https://wiki.sa-mp.com/wiki/db_query
@@ -25,50 +25,50 @@ function db_close( db ){
  * @param {String} query
  * @return {Number} retval
  */
-function db_query( db, query ){
-	var out = CallNativeGDK( "db_query", "is", db, query );
-	return out;
+function db_query(db, query) {
+	return CallNativeGDK("db_query", "is", db, query);
 }
+
 /**
  * db_free_result
  * @see https://wiki.sa-mp.com/wiki/db_free_result
  * @param {Number} dbresult
  * @return {Number} retval
  */
-function db_free_result( dbresult ){
-	var out = CallNativeGDK( "db_free_result", "i", dbresult );
-	return out;
+function db_free_result(dbresult) {
+	return CallNativeGDK("db_free_result", "i", dbresult);
 }
+
 /**
  * db_num_rows
  * @see https://wiki.sa-mp.com/wiki/db_num_rows
  * @param {Number} dbresult
  * @return {Number} retval
  */
-function db_num_rows( dbresult ){
-	var out = CallNativeGDK( "db_num_rows", "i", dbresult );
-	return out;
+function db_num_rows(dbresult) {
+	return CallNativeGDK("db_num_rows", "i", dbresult);
 }
+
 /**
  * db_next_row
  * @see https://wiki.sa-mp.com/wiki/db_next_row
  * @param {Number} dbresult
  * @return {Number} retval
  */
-function db_next_row( dbresult ){
-	var out = CallNativeGDK( "db_next_row", "i", dbresult );
-	return out;
+function db_next_row(dbresult) {
+	return CallNativeGDK("db_next_row", "i", dbresult);
 }
+
 /**
  * db_num_fields
  * @see https://wiki.sa-mp.com/wiki/db_num_fields
  * @param {Number} dbresult
  * @return {Number} retval
  */
-function db_num_fields( dbresult ){
-	var out = CallNativeGDK( "db_num_fields", "i", dbresult );
-	return out;
+function db_num_fields(dbresult) {
+	return CallNativeGDK("db_num_fields", "i", dbresult);
 }
+
 /**
  * db_field_name
  * @see https://wiki.sa-mp.com/wiki/db_field_name
@@ -78,10 +78,10 @@ function db_num_fields( dbresult ){
  * @param {Number} maxlength
  * @return {Number} retval
  */
-function db_field_name( dbresult, field, result, maxlength ){
-	var out = CallNativeGDK( "db_field_name", "iisi", dbresult, field, result, maxlength );
-	return out;
+function db_field_name(dbresult, field, result, maxlength) {
+	return CallNativeGDK("db_field_name", "iisi", dbresult, field, result, maxlength);
 }
+
 /**
  * db_get_field
  * @see https://wiki.sa-mp.com/wiki/db_get_field
@@ -91,10 +91,36 @@ function db_field_name( dbresult, field, result, maxlength ){
  * @param {Number} maxlength
  * @return {Number} retval
  */
-function db_get_field( dbresult, field, result, maxlength ){
-	var out = CallNativeGDK( "db_get_field", "iisi", dbresult, field, result, maxlength );
-	return out;
+function db_get_field(dbresult, field, result, maxlength) {
+	return CallNativeGDK("db_get_field", "iisi", dbresult, field, result, maxlength);
 }
+
+/**
+ * db_get_field_int
+ * @see https://wiki.sa-mp.com/wiki/db_get_field_int
+ * @param {Number} result
+ * @param {Number} [field=0]
+ * @return {Number} retval
+ */
+function db_get_field_int(result, field)
+{
+	field = typeof field === 'undefined' ? 0 : field;
+	return CallNativeGDK("db_get_field_int", "ii", result, field);
+}
+
+/**
+ * db_get_field_float
+ * @see https://wiki.sa-mp.com/wiki/db_get_field_float
+ * @param {Number} result
+ * @param {Number} [field=0]
+ * @return {Number} retval
+ */
+function db_get_field_float(result, field)
+{
+	field = typeof field === 'undefined' ? 0 : field;
+	return CallNativeGDK("db_get_field_float", "ii", result, field);
+}
+
 /**
  * db_get_field_assoc
  * @see https://wiki.sa-mp.com/wiki/db_get_field_assoc
@@ -104,7 +130,66 @@ function db_get_field( dbresult, field, result, maxlength ){
  * @param {Number} maxlength
  * @return {Number} retval
  */
-function db_get_field_assoc( dbresult, field, result, maxlength ){
-	var out = CallNativeGDK( "db_get_field_assoc", "issi", dbresult, field, result, maxlength );
-	return out;
+function db_get_field_assoc(dbresult, field, result, maxlength) {
+	return CallNativeGDK("db_get_field_assoc", "issi", dbresult, field, result, maxlength);
+}
+
+/**
+ * db_get_field_assoc_int
+ * @see https://wiki.sa-mp.com/wiki/db_get_field_assoc_int
+ * @param {Number} result
+ * @param {String} field
+ * @return {Number} retval
+ */
+function db_get_field_assoc_int(result, field) {
+	return CallNativeGDK("db_get_field_assoc_int", "is", result, field);
+}
+
+/**
+ * db_get_field_assoc_float
+ * @see https://wiki.sa-mp.com/wiki/db_get_field_assoc_float
+ * @param {Number} result
+ * @param {String} field
+ * @return {Number} retval
+ */
+function db_get_field_assoc_float(result, field) {
+	return CallNativeGDK("db_get_field_assoc_float", "is", result, field);
+}
+
+/**
+ * db_get_mem_handle
+ * @see https://wiki.sa-mp.com/wiki/db_get_mem_handle
+ * @param {Number} db
+ * @return {Number} retval
+ */
+function db_get_mem_handle(db) {
+	return CallNativeGDK("db_get_mem_handle", "i", db);
+}
+
+/**
+ * db_get_result_mem_handle
+ * @see https://wiki.sa-mp.com/wiki/db_get_result_mem_handle
+ * @param {Number} result
+ * @return {Number} retval
+ */
+function db_get_result_mem_handle(result) {
+	return CallNativeGDK("db_get_result_mem_handle", "i", result);
+}
+
+/**
+ * db_debug_openfiles
+ * @see https://wiki.sa-mp.com/wiki/db_debug_openfiles
+ * @return {Number} retval
+ */
+function db_debug_openfiles() {
+	return CallNativeGDK("db_debug_openfiles");
+}
+
+/**
+ * db_debug_openresults
+ * @see https://wiki.sa-mp.com/wiki/db_debug_openresults
+ * @return {Number} retval
+ */
+function db_debug_openresults() {
+	return CallNativeGDK("db_debug_openresults");
 }
